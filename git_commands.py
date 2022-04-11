9f17236e78a5c1619a684fd50a5e2f8e3ecdba07
Kairat <asyrbekov98@gmail.com>
2022-04-11 15:43:17 +0600

    initial commit.

---
+++ git_commands.py
@@ -0,0 +1,12 @@
+git init .git
+git remove add name url
+git pull oringin master
+git status
+git add
+git commit -m "comment"
+git branch
+git branch name branch
+git checkout name branch
+git push origin master
+git reset filename
+
